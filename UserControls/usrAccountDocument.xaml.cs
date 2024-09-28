09d42781e212164918e39b750086a0cc0df70867
MahdiRastegar <33184923+MahdiRastegar@users.noreply.github.com>
2024-09-28 17:40:54 +0330

    test

--- UserControls/usrAccountDocument.xaml.cs
+++ UserControls/usrAccountDocument.xaml.cs
@@ -802,7 +802,7 @@ namespace WpfCol
             }
             btnCancel_Click(null, null);
         }
-
+        //
         private void SearchTermTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             try
